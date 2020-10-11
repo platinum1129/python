e79173d48011a0be2b759df4486ed6cade5c0160
plati <test@example.com>
2020-10-12 00:29:32 +0900

    slack-zoom-api3

---
+++ MeetingReminderToSlack.py
@@ -0,0 +1,124 @@
+import http.client
+import base64
+import time
+import hmac
+import hashlib
+import urllib
+import json
+import urllib.request
+import datetime
+import os
+
+# from dotenv import load_dotenv
+# load_dotenv()
+
+def get_200_response(message):
+    return {
+        'statusCode': 200,
+        'body': json.dumps(message)
+    }
+
+def lambda_handler(event, context):
+    # holiday decision
+    if is_holiday(datetime.date.today()):
+        return get_200_response('Today is a holiday.')
+
+    ############################################################
+    # Zoom
+    ############################################################
+    join_url = get_meeting_url()
+    
+    ############################################################
+    # Slack
+    ############################################################
+    slack_url = "https://slack.com/api/chat.postMessage"
+    slack_token = os.environ['SLACK_BOT_TOKEN']
+    slack_headers = {
+        "content-type": "application/json; charset=utf-8",
+    	"Authorization": "Bearer " + slack_token
+    }
+    slack_obj = {
+    	"token"		:	slack_token,
+    	# "channel"	:	os.environ['SLACK_CHANNEL_TEST'],	#crd-test
+    	"channel"	:	os.environ['SLACK_CHANNEL_MAIN'],	#crd-circas
+        "text"		:	"<!channel>【Meeting】リマインダー : 朝会9:40～　夕会17:00～　\n　ZoomUrl : " + join_url,
+    }
+    slack_params = json.dumps(slack_obj).encode("utf-8")
+    req = urllib.request.Request(slack_url,slack_params,slack_headers)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+    print(body)
+
+    return get_200_response('ok')
+
+############################################################
+# Zoom
+############################################################
+def get_meeting_url():
+    ZOOM_API_KEY = os.environ['ZOOM_API_KEY']
+    ZOOM_API_SECRET = os.environ['ZOOM_API_SECRET']
+    ZOOM_USER_ID = os.environ['ZOOM_USER_ID']
+
+    expiration = int(time.time()) + 5 # 5sec
+    header    = base64.urlsafe_b64encode('{"alg":"HS256","typ":"JWT"}'.encode()).replace(b'=', b'') 
+    payload   = base64.urlsafe_b64encode(('{"iss":"'+ZOOM_API_KEY+'","exp":"'+str(expiration)+'"}').encode()).replace(b'=', b'') # APIキーと>有効期限
+    hashdata  = hmac.new(ZOOM_API_SECRET.encode(), header+".".encode()+payload, hashlib.sha256) # HMACSHA256でハッシュを作成
+    signature = base64.urlsafe_b64encode(hashdata.digest()).replace(b'=', b'') # ハッシュをURL-Save Base64でエンコード
+    zoom_token = (header+".".encode()+payload+".".encode()+signature).decode()  # トークンをstrで生成
+    conn = http.client.HTTPSConnection("api.zoom.us")
+    zoom_headers = {
+        'authorization': "Bearer "+ zoom_token,
+        'content-type': "application/json; charset=utf-8"
+    }
+    open_time = (datetime.datetime.today() + datetime.timedelta(minutes=1)).strftime("%Y-%m-%dT%H:%M:%S")
+    zoom_obj = {
+        "topic": "Meeting",
+        "type": 2,	#1:即時 2:時間指定（時間指定でないとホスト抜き不可）
+        "start_time": open_time,
+        "timezone": "Asia/Tokyo",
+        "settings": {
+            "host_video" : "false",
+            "participant_video" : "false",
+            "join_before_host" : "true",
+            "use_pmi": "false",
+            "approval_type" : "0",
+            "waiting_room" : "false"
+        }
+    }
+    zoom_params = json.dumps(zoom_obj).encode("utf-8")
+    zoom_url = "https://api.zoom.us/v2/users/" + ZOOM_USER_ID + "/meetings"
+    conn.request("POST", zoom_url, zoom_params, zoom_headers)
+    res = conn.getresponse()
+    data = res.read()
+    dic = json.loads(data.decode("utf-8"))
+    return dic["join_url"]
+
+############################################################
+# 祝日判定
+############################################################
+def is_holiday(today):
+    url = "https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv"
+    with urllib.request.urlopen(url) as res:
+        mem = res.read()
+
+    holidays = {}
+    for row in mem.decode('cp932').splitlines():
+        arr = row.split(",")
+        day, name = arr[0], arr[1]
+        holidays[day] = {'day': day, 'name': name}
+
+    # 年末年始定休日
+    add_company_holiday(holidays, '2020/12/30')
+    add_company_holiday(holidays, '2020/12/31')
+    add_company_holiday(holidays, '2021/1/2')
+    add_company_holiday(holidays, '2021/1/3')
+    add_company_holiday(holidays, '2021/1/4')
+
+    return today.strftime('%Y/%m/%d') in holidays
+
+def add_company_holiday(holidays, day):
+    holidays[day] = {'day': day, 'name': '定休日'}
+
+if __name__ == "__main__":
+    res = lambda_handler({},{})
+    print(res)
---
+++ PostZoomMeeting.py
@@ -0,0 +1,185 @@
+import os
+import json
+import time
+import hmac
+import http.client
+import base64
+import urllib
+import urllib.request
+import logging
+import hashlib
+import datetime
+
+# from dotenv import load_dotenv
+# load_dotenv()
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+SLACK_OAUTH_TOKEN = os.environ['SLACK_OAUTH_TOKEN']
+SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+SLACK_USER_NAME = os.environ['SLACK_USER_NAME']
+ZOOM_API_KEY = os.environ['ZOOM_API_KEY']
+ZOOM_API_SECRET = os.environ['ZOOM_API_SECRET']
+ZOOM_USER_ID = os.environ['ZOOM_USER_ID']
+
+def lambda_handler(event, context):
+
+    #受信したjsonをLogsに出力
+    logging.info(json.dumps(event))
+
+    # Slackからのリトライの場合は処理無視
+    if 'x-slack-retry-num' in event.get('headers'):
+        return {'statusCode': 200}
+
+    # json処理
+    if 'body' in event:
+        body = json.loads(event.get('body'))
+    elif 'token' in event:
+        body = event
+    else:
+        logger.error('unexpected event format')
+        return {'statusCode': 500, 'body': 'error:unexpected event format'}
+    
+    if 'challenge' in body:
+        challenge = body.get('challenge')
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+            	'challenge' : challenge
+            })
+        }
+    
+    #SlackMessageに「zoom」が入っていたときの処理
+    body_text = body.get('event').get('text')
+    logging.info("body_text:" + body_text)
+    if 'zoom' in body_text:
+        host_user = get_email(body.get('event').get('user'))
+        logging.info("host_user:" + host_user)
+        join_url = get_meeting_url(host_user)
+        logging.info("join_url:" + join_url)
+
+        ts = None
+        if ('thread_ts' in body.get('event')):
+            # thread_ts は スレッドのルート
+            ts = body.get('event').get('thread_ts')
+        else:
+            # スレッド中からの呼び出しじゃないと、thread_tsは無いので、tsを指定
+            ts = body.get('event').get('ts')
+
+        url = 'https://slack.com/api/chat.postMessage'
+        headers = {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer {0}'.format(SLACK_BOT_TOKEN)
+        }
+        data = {
+            'token': SLACK_OAUTH_TOKEN,
+            'channel': body.get('event').get('channel'),
+            'text': 'ZoomUrl : ' + join_url,
+            'username': SLACK_USER_NAME,
+            'thread_ts': ts
+        }
+        # POST処理　
+        req = urllib.request.Request(url, data=json.dumps(data).encode('utf-8'), method='POST', headers=headers)
+        res = urllib.request.urlopen(req)
+        logger.info('post result: %s', res.msg)
+        return {
+            'statusCode': 200,
+            'headers': {},
+            'body': json.dumps({
+                'text' : "OK",
+                'join_url' : join_url
+            })
+        }
+        
+    return {
+        'statusCode': 200,
+        'headers': {},
+        'body': "noting"
+    }
+
+
+def get_email(slack_user_id):
+    logging.info("slack_user_id:"+ slack_user_id)
+    slack_token = os.environ['SLACK_BOT_TOKEN']
+    slack_url = "https://slack.com/api/users.info?token=" + slack_token + "&user=" + slack_user_id
+    req = urllib.request.Request(slack_url)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+
+    dic = json.loads(body.decode("utf-8"))
+    email = dic["user"]["profile"]["email"]
+    logging.info("email:"+ email)
+
+    return email
+
+############################################################
+# Zoom
+############################################################
+def get_meeting_url(host_user):
+    
+    if isWithinTheTeam(host_user):
+        zoom_user_id = host_user        # Zoomのチーム内であれば自身をホスト
+    else:
+        zoom_user_id = ZOOM_USER_ID     # Zoomのチーム外であればデフォルトをホスト
+    
+    open_time = (datetime.datetime.today() + datetime.timedelta(minutes=1)).strftime("%Y-%m-%dT%H:%M:%S")
+    zoom_obj = {
+        "topic": zoom_user_id + " Meeting",
+        "type": 2,	#1:即時 2:時間指定（時間指定でないとホスト抜き不可）
+        "start_time": open_time,
+        "timezone": "Asia/Tokyo",
+        "settings": {
+            "host_video" : "false",
+            "participant_video" : "false",
+            "join_before_host" : "true",
+            "use_pmi": "false",
+            "approval_type" : "0",
+            "waiting_room" : "false"
+        }
+    }
+    zoom_params = json.dumps(zoom_obj).encode("utf-8")
+
+    zoom_url = "https://api.zoom.us/v2/users/" + zoom_user_id + "/meetings"    
+    conn = http.client.HTTPSConnection("api.zoom.us")
+    conn.request("POST", zoom_url, zoom_params, headers=get_zoom_headers())
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    logging.info(data)
+    dic = json.loads(data)
+    return dic["join_url"]
+
+
+def isWithinTheTeam(zoom_user_id):
+    zoom_url = "https://api.zoom.us/v2/users/" + zoom_user_id
+    conn = http.client.HTTPSConnection("api.zoom.us")
+    conn.request("GET", zoom_url, headers=get_zoom_headers())
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    res.close()
+    logging.info(data)
+    logging.info(res.status)
+    return res.status == 200
+
+
+def get_zoom_headers():
+    expiration = int(time.time()) + 5 # 5sec
+    header = base64.urlsafe_b64encode('{"alg":"HS256","typ":"JWT"}'.encode()).replace(b'=', b'') 
+    payload = base64.urlsafe_b64encode(('{"iss":"'+ZOOM_API_KEY+'","exp":"'+str(expiration)+'"}').encode()).replace(b'=', b'') # APIキーと>有効期限
+    hashdata = hmac.new(ZOOM_API_SECRET.encode(), header+".".encode()+payload, hashlib.sha256) # HMACSHA256でハッシュを作成
+    signature = base64.urlsafe_b64encode(hashdata.digest()).replace(b'=', b'') # ハッシュをURL-Save Base64でエンコード
+    zoom_token = (header+".".encode()+payload+".".encode()+signature).decode()  # トークンをstrで生成
+    zoom_headers = {
+        'authorization': "Bearer "+ zoom_token,
+        'content-type': "application/json"
+    }
+    return zoom_headers
+
+
+if __name__ == "__main__":
+    lambda_handler({
+        "token": "testtoken",
+        "challenge": "testchallenge",
+        "type": "url_verification"
+    },
+    {})
